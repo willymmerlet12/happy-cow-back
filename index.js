db2f8836beaedb8765933df534a832fc2449d00b
Willy <willymmerlet@gmail.com>
2020-12-09 18:25:23 +0100

    ert

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(formidable());
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
